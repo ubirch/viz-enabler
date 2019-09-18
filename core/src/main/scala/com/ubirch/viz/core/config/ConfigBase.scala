44f5ae58740bc5517f9e7fc0250bf64bd2be785a
lotta <lotta.rueger@ubirch.com>
2019-09-18 15:18:26 +0200

    added esClient and configuration

--- core/src/main/scala/com/ubirch/viz/core/config/ConfigBase.scala
+++ core/src/main/scala/com/ubirch/viz/core/config/ConfigBase.scala
@@ -3,5 +3,14 @@ package com.ubirch.viz.core.config
 import com.typesafe.config.{Config, ConfigFactory}
 
 trait ConfigBase {
-  def conf: Config = ConfigFactory.load()
+
+  private def conf: Config = ConfigFactory.load()
+
+  val protocol: String = conf.getString("vizEnabler.elasticSearch.protocol")
+  val host: String = conf.getString("vizEnabler.elasticSearch.host")
+  val port: Int = conf.getInt("vizEnabler.elasticSearch.port")
+  val password: String = conf.getString("vizEnabler.elasticSearch.io_password")
+  val username: String = conf.getString("vizEnabler.elasticSearch.io_user")
+  val index: String = conf.getString("vizEnabler.elasticSearch.deviceData.index")
+
 }
---
+++ core/src/main/scala/com/ubirch/viz/core/elastic/EsClient.scala
@@ -0,0 +1,71 @@
+package com.ubirch.viz.core.elastic
+
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.alpakka.elasticsearch.scaladsl.ElasticsearchFlow
+import akka.stream.alpakka.elasticsearch.{ElasticsearchSourceSettings, ElasticsearchWriteSettings, WriteMessage}
+import akka.stream.scaladsl.{Sink, Source}
+import com.typesafe.scalalogging.LazyLogging
+import com.ubirch.viz.core.config.ConfigBase
+import org.apache.http.HttpHost
+import org.apache.http.auth.{AuthScope, UsernamePasswordCredentials}
+import org.apache.http.impl.client.BasicCredentialsProvider
+import org.apache.http.impl.io.AbstractMessageWriter
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder
+import org.elasticsearch.client.RestClient
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+import scala.util.{Failure, Success}
+
+class EsClient extends LazyLogging with ConfigBase {
+
+  val credentialsProvider = new BasicCredentialsProvider
+  credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password))
+
+  protected lazy implicit val client: RestClient = if (protocol.contains("https")) {
+    RestClient.builder(new HttpHost(host, port, protocol))
+      .setHttpClientConfigCallback((httpClientBuilder: HttpAsyncClientBuilder) =>
+        httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider)).build()
+  } else {
+    RestClient.builder(new HttpHost(host, port, protocol)).build()
+  }
+
+  private implicit val system: ActorSystem = ActorSystem()
+  private implicit val materializer: ActorMaterializer = ActorMaterializer()
+
+  private implicit val sourceSettings: ElasticsearchSourceSettings = ElasticsearchSourceSettings()
+    .withBufferSize(10000)
+    .withScrollDuration(1.minutes)
+
+
+  /**
+    * This method is executing a scroll search on the elasticsearch.
+    *
+    * @param jsonData The query of the scroll search.
+    * @return an Akka Stream Source with the results
+    */
+  def storeDeviceData(jsonData: String): Unit = {
+
+    Source(
+      collection.immutable.Seq(
+        WriteMessage.createIndexMessage(jsonData),
+      )
+    ).via(
+      ElasticsearchFlow.create(
+        indexName = index,
+        typeName = "_doc",
+        ElasticsearchWriteSettings.Default,
+        AbstractMessageWriter[String]
+      )
+    ).runWith(Sink.seq)
+      .onComplete {
+        case Success(_) =>
+          logger.info(s"successfully writing $jsonData to elasticsearch.")
+        case Failure(ex) =>
+          logger.error(s"error while writing $jsonData to elasticsearch: ", ex)
+      }
+  }
+
+
+}
