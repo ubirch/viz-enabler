9cb8c9a111bcce1af93ce35601144eb08b62a669
lotta <lotta.rueger@ubirch.com>
2019-09-24 07:56:03 +0200

    removed cors allowed origin only as localhost

--- server/src/main/scala/ScalatraBootstrap.scala
+++ server/src/main/scala/ScalatraBootstrap.scala
@@ -7,7 +7,7 @@ class ScalatraBootstrap extends LifeCycle {
   implicit val swagger: ApiSwagger = new ApiSwagger
 
   override def init(context: ServletContext) {
-    context.initParameters("org.scalatra.cors.allowedOrigins") = "http://0.0.0.0"
+
     context.mount(new ApiRest, "/", "RestApi")
     context.mount(new ResourcesApp, "/api-docs")
   }
